0a7d0b53ddeba7007b725a4e27a38fd3bf68bfa6
Josue <josue.chaqui@gmail.com>
2013-04-01 18:33:47 -0600

    iniciando un nuevo proyecto

---
+++ src/logicaArbol/Usuario.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logicaArbol;
+
+/**
+ *
+ * @author chaqui
+ */
+public class Usuario {
+    
+}
